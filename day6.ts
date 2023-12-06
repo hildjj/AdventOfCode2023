38eeba7cb5fca7cef6580610c88340bc71edc9c4
Joe Hildebrand <joe-github@cursive.net>
2023-12-06 07:23:58 -0700

    Now with perf

--- day6.ts
+++ day6.ts
@@ -17,18 +17,43 @@ function part1(inp: TimeDistance[]): number {
   }).reduce((t, v) => t * v, 1);
 }
 
+/**
+ * Find the place between start and end where fn(t) changes values.
+ */
+function findChange(
+  start: bigint,
+  end: bigint,
+  fn: (t: bigint) => boolean,
+): bigint {
+  while (start < end) {
+    const t2 = (start + end) / 2n;
+    if (fn(t2)) {
+      if (!fn(t2 + 1n)) {
+        return t2;
+      }
+      start = t2;
+    } else {
+      end = t2;
+    }
+  }
+
+  throw new Error('Not found');
+}
+
 function part2(inp: TimeDistance[]): number {
   const time = BigInt(inp.reduce((t, v) => t + String(v.time), ''));
   const dist = BigInt(inp.reduce((t, v) => t + String(v.dist), ''));
 
-  // TODO(hildjj): binary seach for the edges
-  let count = 0;
-  for (let i = 0n; i < time; i++) {
-    if (i * (time - i) > dist) {
-      count++;
-    }
+  // Confirmed that there is only one range, which covers the midpoint.
+  // Approach: start at midpoint and work out.
+  const t2 = time / 2n;
+  if (t2 * (time - t2) <= dist) {
+    throw new Error('Range not near half');
   }
-  return count;
+  const top = findChange(t2, time, (t) => t * (time - t) > dist);
+  const bottom = findChange(0n, t2, (t) => t * (time - t) <= dist);
+
+  return Number(top - bottom);
 }
 
 export default async function main(args: MainArgs): Promise<[number, number]> {
